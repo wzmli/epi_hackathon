0891f01e403c2e2766f00734771267953b0ce9cc
wzmli <wzmichael.li@gmail.com>
2023-05-26 10:11:45 -0500

    adding time varying simulations

--- SEIR/simulation.R
+++ SEIR/simulation.R
@@ -28,9 +28,11 @@ sim_results = mod_simulator$report()
 head(sim_results)
 sim_results
 
-(sim_results %>% filter(matrix == "total_inflow")
+(sim_results %>% filter(matrix == "total_inflow") %>% filter(row == "I")
 %>% ggplot()
 	+ geom_line(aes(time, value, colour = row))
 	+ labs(colour = "state")
 	+ theme_bw()
 )
+
+
