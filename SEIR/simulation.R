a285394c1e892250f117b373837e7774ff626ed4
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-05-26 11:27:42 -0400

    incidence

--- SEIR/simulation.R
+++ SEIR/simulation.R
@@ -8,11 +8,12 @@ params = ("SEIR/default.csv"
 	%>% with(setNames(Default, Variable))
 )
 
-
-mod_simulator = model$simulators$tmb(time_steps = 100
-	, state = c(S = params[["S"]], E = params[["E"]]
+state = c(S = params[["S"]], E = params[["E"]]
 							, I = params[["I"]], H = params[["H"]]
 							, R = params[["R"]], D = params[["D"]])
+
+mod_simulator = model$simulators$tmb(time_steps = 100
+  , state = state
 	, flow = c(infection = params[["infection"]], progression = params[["progression"]]
 					 , hospitalization = params[["hospitalization"]]
 					 , discharge = params[["discharge"]], recovery = params[["recovery"]]
@@ -20,6 +21,8 @@ mod_simulator = model$simulators$tmb(time_steps = 100
 					 )
 	, transmission = params[["transmission"]]
 	, N = sum(as.numeric(params[c("S","E","I","H","R","D")])) # explained below
+	, .mats_to_return = c("state", "total_inflow")
+	, .dimnames = list(total_inflow = list(names(state), ""))
 	# , state = c(S = 998, E = 1, I = 1, H = 0, R = 0)
 	# , flow = c(infection = 0.2
 	# 					 , progression = 0.5, recovery = 0.2
@@ -27,7 +30,7 @@ mod_simulator = model$simulators$tmb(time_steps = 100
 	# )
 	# , transmission = 1
 	# , N = 1000 # explained below
-	
+
 )
 
 sim_results = mod_simulator$report()
